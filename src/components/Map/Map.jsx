0f2bb0ca3ef44915f81043c6381ee967ea602970
Shivam Kumar <smileyshivam3042@gmail.com>
2022-01-30 19:31:46 +0530

    header added with other styles for components

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles'
+
+const Map = () => {
+    return (
+        <h1>Map</h1>
+    )
+}
+
+export default Map;
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles'
+
+const PlaceDetails = () => {
+    return (
+        <h1>PlaceDetails</h1>
+    )
+}
+
+export default PlaceDetails;
